c1a535274337a7a4371253c31ab1cf3f6c4043fd
Ashish Mishra <ashish@gooru.org>
2015-12-16 14:07:46 +0530

    Provide a constant string for auth handler path mapping.

--- src/main/java/org/gooru/nucleus/gateway/constants/RouteConstants.java
+++ src/main/java/org/gooru/nucleus/gateway/constants/RouteConstants.java
@@ -8,6 +8,7 @@ public class RouteConstants {
   // Helper constants
   public static final String API_VERSION = "v1";
   public static final String API_BASE_ROUTE = "/api/nucleus/" + API_VERSION + "/";
+  public static final String API_AUTH_ROUTE = "/api/nucleus/*";
 
   // Helper: Entity name constants
   public static final String ENTITY_RESOURCES = "resources";
