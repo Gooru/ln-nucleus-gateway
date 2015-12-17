58b21f297fc1d234dca225c29f69585944ccd72d
Ashish Mishra <ashish@gooru.org>
2015-12-17 11:01:19 +0530

    Add one more constant value for anonymous user.

--- src/main/java/org/gooru/nucleus/gateway/constants/MessageConstants.java
+++ src/main/java/org/gooru/nucleus/gateway/constants/MessageConstants.java
@@ -8,5 +8,6 @@ public class MessageConstants {
   public static final String MSG_OP_STATUS = "mb.operation.status";
   public static final String MSG_KEY_PREFS = "prefs";
   public static final String MSG_OP_STATUS_SUCCESS = "success";
+  public static final String MSG_USER_ANONYMOUS = "anonymous";
 
 }
