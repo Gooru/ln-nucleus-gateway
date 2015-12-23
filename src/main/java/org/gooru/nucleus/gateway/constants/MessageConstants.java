7ac5096dd86dbb2eb2171129d3cccba86864004f
Ashish Mishra <ashish@gooru.org>
2015-12-23 07:11:43 +0530

    Add resource handler specific constants.

--- src/main/java/org/gooru/nucleus/gateway/constants/MessageConstants.java
+++ src/main/java/org/gooru/nucleus/gateway/constants/MessageConstants.java
@@ -18,5 +18,11 @@ public class MessageConstants {
   public static final String MSG_HTTP_ERROR = "http.error";
   public static final String MSG_HTTP_VALIDATION_ERROR = "http.validation.error";
   public static final String MSG_HTTP_HEADERS = "http.headers";
+  
+  // Operation names: Also need to be updated in corresponding handlers
+  public static final String MSG_OP_RES_GET = "resource.get";
+  public static final String MSG_OP_RES_CREATE = "resource.create";
+  public static final String MSG_OP_RES_UPDATE = "resource.update";
+  
 
 }
