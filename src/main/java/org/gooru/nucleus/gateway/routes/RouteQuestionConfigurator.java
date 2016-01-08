fecffbc3a0ee67ef7bb4721eaca34ddb84491672
Ashish Mishra <ashish@gooru.org>
2016-01-08 13:55:24 +0530

    Provide handler for update of resource

---
+++ src/main/java/org/gooru/nucleus/gateway/routes/RouteQuestionConfigurator.java
@@ -0,0 +1,7 @@
+package org.gooru.nucleus.gateway.routes;
+
+/**
+ * Created by ashish on 8/1/16.
+ */
+public class RouteQuestionConfigurator {
+}
--- src/main/java/org/gooru/nucleus/gateway/routes/RouteResourceConfigurator.java
+++ src/main/java/org/gooru/nucleus/gateway/routes/RouteResourceConfigurator.java
@@ -1,12 +1,9 @@
 package org.gooru.nucleus.gateway.routes;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.eventbus.Message;
 import org.gooru.nucleus.gateway.constants.ConfigConstants;
 import org.gooru.nucleus.gateway.constants.MessageConstants;
 import org.gooru.nucleus.gateway.constants.MessagebusEndpoints;
 import org.gooru.nucleus.gateway.constants.RouteConstants;
-import org.gooru.nucleus.gateway.responses.writers.ResponseWriterBuilder;
 import org.gooru.nucleus.gateway.routes.utils.RouteRequestUtility;
 import org.gooru.nucleus.gateway.routes.utils.RouteResponseUtility;
 import org.slf4j.Logger;
@@ -15,46 +12,50 @@ import org.slf4j.LoggerFactory;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
 
 class RouteResourceConfigurator implements RouteConfigurator {
 
-  static final Logger LOG = LoggerFactory.getLogger("org.gooru.nucleus.gateway.bootstrap.ServerVerticle");
+  static final Logger LOGGER = LoggerFactory.getLogger("org.gooru.nucleus.gateway.bootstrap.ServerVerticle");
 
   @Override
   public void configureRoutes(Vertx vertx, Router router, JsonObject config) {
-    
+
     final EventBus eb = vertx.eventBus();
 
     final long mbusTimeout = config.getLong(ConfigConstants.MBUS_TIMEOUT, 30L);
 
-    
+
     router.get(RouteConstants.EP_RESOURCE_GET).handler(routingContext -> {
       String resourceId = routingContext.request().getParam(RouteConstants.ID_RESOURCE);
-      DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout*1000).addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RES_GET)
-              .addHeader(RouteConstants.ID_RESOURCE, resourceId);
+      DeliveryOptions options =
+        new DeliveryOptions().setSendTimeout(mbusTimeout * 1000).addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RES_GET)
+                             .addHeader(RouteConstants.ID_RESOURCE, resourceId);
       eb.send(MessagebusEndpoints.MBEP_RESOURCE, new RouteRequestUtility().getBodyForMessage(routingContext), options, reply -> {
-        new RouteResponseUtility().responseHandler(routingContext, reply, LOG);
+        new RouteResponseUtility().responseHandler(routingContext, reply, LOGGER);
       });
     });
-    
+
     router.post(RouteConstants.EP_RESOURCE_CREATE).handler(routingContext -> {
       DeliveryOptions options =
         new DeliveryOptions().setSendTimeout(mbusTimeout * 1000).addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RES_CREATE);
       eb.send(MessagebusEndpoints.MBEP_RESOURCE, new RouteRequestUtility().getBodyForMessage(routingContext), options, reply -> {
-        new RouteResponseUtility().responseHandler(routingContext, reply, LOG);
-      });      
+        new RouteResponseUtility().responseHandler(routingContext, reply, LOGGER);
+      });
     });
-    
-    
+
+
     router.put(RouteConstants.EP_RESOURCE_UPDATE).handler(routingContext -> {
-      
+      String resourceId = routingContext.request().getParam(RouteConstants.ID_RESOURCE);
+      DeliveryOptions options =
+        new DeliveryOptions().setSendTimeout(mbusTimeout * 1000).addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RES_UPDATE)
+                             .addHeader(RouteConstants.ID_RESOURCE, resourceId);
+      eb.send(MessagebusEndpoints.MBEP_RESOURCE, new RouteRequestUtility().getBodyForMessage(routingContext), options, reply -> {
+        new RouteResponseUtility().responseHandler(routingContext, reply, LOGGER);
+      });
     });
   }
-
 
 
 }
