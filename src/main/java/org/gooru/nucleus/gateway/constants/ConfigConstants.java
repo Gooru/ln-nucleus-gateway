ea3285f1540ca1ae30e96c775591945982e5970e
am-gooru <ashish@gooru.org>
2016-02-02 14:42:44 +0530

    Correct the spelling

--- src/main/java/org/gooru/nucleus/gateway/constants/ConfigConstants.java
+++ src/main/java/org/gooru/nucleus/gateway/constants/ConfigConstants.java
@@ -8,7 +8,7 @@ public class ConfigConstants {
 
   public static final String HTTP_PORT = "http.port";
   public static final String METRICS_PERIODICITY = "metrics.periodicity.seconds";
-  public static final String MBUS_TIMEOUT = "message.bus.send.timout.seconds";
+  public static final String MBUS_TIMEOUT = "message.bus.send.timeout.seconds";
   public static final String MAX_REQ_BODY_SIZE = "request.body.size.max.mb";
 
 }
