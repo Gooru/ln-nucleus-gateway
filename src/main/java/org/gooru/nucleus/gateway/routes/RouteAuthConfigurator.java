7a5fa691c06b78ca99e333a36807aaba411ae960
am-gooru <ashish@gooru.org>
2016-02-14 07:41:47 +0530

    Spell correction

--- src/main/java/org/gooru/nucleus/gateway/routes/RouteAuthConfigurator.java
+++ src/main/java/org/gooru/nucleus/gateway/routes/RouteAuthConfigurator.java
@@ -70,7 +70,7 @@ class RouteAuthConfigurator implements RouteConfigurator {
 
   private String extractSessionToken(String authHeader) {
     if (authHeader == null || authHeader.isEmpty()) {
-      LOG.debug("Session tokenq is null or empty");
+      LOG.debug("Session token is null or empty");
       return null;
     }
     Matcher authMatcher = AUTH_PATTERN.matcher(authHeader);
