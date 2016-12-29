88dceeace9d19623b6bb409ba902c4b3c1476bcf
Praveen Pandey <ppandey@pragyasystems.com>
2016-12-29 15:24:04 +0530

    Modified for API Versioning

--- src/main/java/org/gooru/nucleus/gateway/routes/RouteKeywordConfigurator.java
+++ src/main/java/org/gooru/nucleus/gateway/routes/RouteKeywordConfigurator.java
@@ -1,19 +1,20 @@
 package org.gooru.nucleus.gateway.routes;
 
-import org.gooru.nucleus.gateway.constants.ConfigConstants;
-import org.gooru.nucleus.gateway.constants.MessageConstants;
-import org.gooru.nucleus.gateway.constants.MessagebusEndpoints;
-import org.gooru.nucleus.gateway.constants.RouteConstants;
-import org.gooru.nucleus.gateway.routes.utils.RouteRequestUtility;
-import org.gooru.nucleus.gateway.routes.utils.RouteResponseUtility;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+
+import org.gooru.nucleus.gateway.constants.ConfigConstants;
+import org.gooru.nucleus.gateway.constants.MessageConstants;
+import org.gooru.nucleus.gateway.constants.MessagebusEndpoints;
+import org.gooru.nucleus.gateway.constants.RouteConstants;
+import org.gooru.nucleus.gateway.routes.utils.DeliveryOptionsBuilder;
+import org.gooru.nucleus.gateway.routes.utils.RouteRequestUtility;
+import org.gooru.nucleus.gateway.routes.utils.RouteResponseUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RouteKeywordConfigurator implements RouteConfigurator {
 
@@ -30,7 +31,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
         router.get(RouteConstants.EP_COURSE_KEYWORD).handler(routingContext -> {
             String courseId = routingContext.request().getParam(RouteConstants.ID_COURSE);
             LOGGER.debug("Getting keyword for course");
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_COURSE_KEYWORD_GET)
                 .addHeader(RouteConstants.ID_COURSE, courseId);
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
@@ -42,7 +44,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
             String resourceId = routingContext.request().getParam(RouteConstants.ID_RESOURCE);
             LOGGER.debug("Getting keyword for resource");
             LOGGER.debug("Routing resource ID : " + resourceId);
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RESOURCE_KEYWORD_GET)
                 .addHeader(RouteConstants.ID_RESOURCE, resourceId);
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
@@ -53,7 +56,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
         router.get(RouteConstants.EP_COLLECTION_KEYWORD).handler(routingContext -> {
             String collectionId = routingContext.request().getParam(RouteConstants.ID_COLLECTION);
             LOGGER.debug("Getting keyword for collection");
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_COLLECTION_KEYWORD_GET)
                 .addHeader(RouteConstants.ID_COLLECTION, collectionId);
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
@@ -62,7 +66,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
 
         router.put(RouteConstants.EP_RESOURCE_KEYWORD).handler(routingContext -> {
             String resourceId = routingContext.request().getParam(RouteConstants.ID_RESOURCE);
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RESOURCE_KEYWORD_UPDATE)
                 .addHeader(RouteConstants.ID_RESOURCE, resourceId);
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
@@ -71,7 +76,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
 
         router.put(RouteConstants.EP_COURSE_KEYWORD).handler(routingContext -> {
             String courseId = routingContext.request().getParam(RouteConstants.ID_COURSE);
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_COURSE_KEYWORD_UPDATE)
                 .addHeader(RouteConstants.ID_COURSE, courseId);
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
@@ -80,7 +86,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
 
         router.put(RouteConstants.EP_COLLECTION_KEYWORD).handler(routingContext -> {
             String collectionId = routingContext.request().getParam(RouteConstants.ID_COLLECTION);
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_COLLECTION_KEYWORD_UPDATE)
                 .addHeader(RouteConstants.ID_COLLECTION, collectionId);
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
@@ -88,7 +95,8 @@ public class RouteKeywordConfigurator implements RouteConfigurator {
         });
         
         router.put(RouteConstants.EP_RESOURCE_KEYWORD_BULK_UPDATE).handler(routingContext -> {
-            DeliveryOptions options = new DeliveryOptions().setSendTimeout(mbusTimeout * 1000)
+            DeliveryOptions options = 
+                DeliveryOptionsBuilder.buildWithApiVersion(routingContext).setSendTimeout(mbusTimeout * 1000)
                 .addHeader(MessageConstants.MSG_HEADER_OP, MessageConstants.MSG_OP_RESOURCE_KEYWORD_UPDATE)
                 .addHeader(MessageConstants.MSG_HEADER_BULK_UPDATE, String.valueOf(Boolean.TRUE));
             eb.send(MessagebusEndpoints.MBEP_KEYWORD, new RouteRequestUtility().getBodyForMessage(routingContext),
